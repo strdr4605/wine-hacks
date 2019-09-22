2d8e1aaf1c05456c1174628ed6861efd75d4ee4f
Dragoș Străinu <str.dr4605@gmail.com>
2019-09-22 11:12:34 +0300

    Format fixes

--- src/components/WineItem/WineItem.tsx
+++ src/components/WineItem/WineItem.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import { IVintage } from '../../types';
-import './WineItem.css';
+import React from "react";
+import { IVintage } from "../../types";
+import "./WineItem.css";
 
 interface Props {
-    vintage: IVintage;
-    onClick(name: string): void;
+  vintage: IVintage;
+  onClick(name: string): void;
 }
 
-export const WineItem: React.FC<Props> = ({ vintage, onClick }) => (
-    <div className="wine-item">
-        <div className="wine-details">
-            <img src={vintage.photo.url} alt="" />
-            <p>Alcohol is: {vintage.alcohol}</p>
-            <p>Sugar is: {vintage.alcohol}</p>
-            <p>Year: {vintage.year}</p>
-        </div>
-        <button className="btn wine-vote" onClick={() => onClick(vintage.photo.name)}>Vote this Wine</button>
-    </div>)
+export const WineItem: React.FC<Props> = ({
+  vintage,
+  onClick
+}: Props): React.ReactElement => (
+  <div className="wine-item">
+    <div className="wine-details">
+      <img src={vintage.photo.url} alt="" />
+      <p>Alcohol is: {vintage.alcohol}</p>
+      <p>Sugar is: {vintage.alcohol}</p>
+      <p>Year: {vintage.year}</p>
+    </div>
+    <button
+      className="btn wine-vote"
+      onClick={(): void => onClick(vintage.photo.name)}
+    >
+      Vote this Wine
+    </button>
+  </div>
+);
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,25 +1,24 @@
-import { IRootState, IAction, ActionTypeEnum } from "./types";
-import { stat } from "fs";
+import { ActionTypeEnum, IAction, IRootState } from "./types";
 
 export const initialState: IRootState = {
   wineList: {},
   questions: [],
   score: 0,
   round: [],
-}
+};
 
-export function reducer(state: IRootState, action: IAction) {
+export function reducer(state: IRootState, action: IAction): IRootState {
   switch (action.type) {
     case ActionTypeEnum.FETCH_VINTAGES_SUCCESS:
       return {
         ...state,
-        wineList: action.payload ? action.payload : {},
-      }
-      case ActionTypeEnum.UPDATE_ROUND:
-        return{
-          ...state,
-          round: action.payload ? action.payload : [],
-        }
+        wineList: action.payload ? action.payload : {}
+      };
+    case ActionTypeEnum.UPDATE_ROUND:
+      return {
+        ...state,
+        round: action.payload ? action.payload : []
+      };
     default:
       return state;
   }
