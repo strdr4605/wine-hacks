441f5fca551d2e9787aa4e54cc71e12adcc755fc
Dragoș Străinu <str.dr4605@gmail.com>
2019-09-22 16:19:23 +0300

    Add wine title

--- src/components/WineItem/WineItem.tsx
+++ src/components/WineItem/WineItem.tsx
@@ -1,3 +1,4 @@
+import changeCase from "change-case";
 import React from "react";
 import { IVintage } from "../../types";
 import "./WineItem.css";
@@ -10,18 +11,37 @@ interface Props {
 
 const wineInfo = (vintage: IVintage) => (
   <div>
-    <p>Alcohol is: <strong>{vintage.alcohol}</strong></p>
-    <p>Sugar is:  <strong>{vintage.alcohol}</strong></p>
-    <p>Year:  <strong>{vintage.year}</strong></p>
-    <p>Wine type:  <strong>{vintage.categorySimple.find(e => e)}</strong></p>
+    <p>
+      Alcohol is: <strong>{vintage.alcohol}</strong>
+    </p>
+    <p>
+      Sugar is: <strong>{vintage.alcohol}</strong>
+    </p>
+    <p>
+      Year: <strong>{vintage.year}</strong>
+    </p>
+    <p>
+      Wine type: <strong>{vintage.categorySimple.find(e => e)}</strong>
+    </p>
   </div>
-)
+);
+
+function getName(vintage: IVintage) {
+  const kebabCaseName: string = vintage.photo
+    ? vintage.photo.name.split(".")[0]
+    : "No Name";
+  return changeCase.titleCase(kebabCaseName);
+}
 
 export const WineItem: React.FC<Props> = ({ vintage, onClick, showInfo }) => (
   <div className="wine-item">
     <div className="wine-details">
       <img src={vintage.photo.url} alt="" />
+      <p>{getName(vintage)}</p>
       {showInfo && wineInfo(vintage)}
     </div>
-    <button className="btn wine-vote" onClick={() => onClick(vintage.id)}>Vote this Wine</button>
-  </div>)
+    <button className="btn wine-vote" onClick={() => onClick(vintage.id)}>
+      Vote this Wine
+    </button>
+  </div>
+);
