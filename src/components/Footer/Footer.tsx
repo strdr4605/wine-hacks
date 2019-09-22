ef65c092d8d19bef1405c0128da127e0574c1973
Dragoș Străinu <str.dr4605@gmail.com>
2019-09-22 13:46:30 +0300

    Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+export const Footer: React.FC<{}> = () => (
+  <footer>
+    <a href="https://github.com/strdr4605/wine-hacks">WineQuiz</a> Made by{" "}
+    <a href="https://github.com/strdr4605">Dragoș Străinu</a>,{" "}
+    <a href="https://github.com/CapceleaCristian">Capcelea Cristian</a>,{" "}
+    <a href="https://github.com/Andreeii">Țurcanu Andrei</a> at{" "}
+    <a href="https://www.facebook.com/events/2390607614550967/">
+      Wine Hacks 2.0
+    </a>
+  </footer>
+);
